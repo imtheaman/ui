6dbb8144464424a8eafe8f3df26b58657297e062
Aman Kumar <aman.kumar@touch.black>
2024-04-10 14:59:37 +0530

    feat: add theme values

--- theme.ts
+++ theme.ts
@@ -1,12 +1,33 @@
- export const darkTheme = {
-   colors: {
-     typography: '#ffffff',
-     background: '#000000'
-   },
-   margins: {
-     sm: 2,
-     md: 4,
-     lg: 8,
-     xl: 12
-   }
- } as const
+export const darkTheme = {
+	colors: {
+		typography: '#ffffff',
+		typographyLight: '#555',
+		backgroundLightBlack: '#1C1A1F',
+		backgroundDarkBlack: '#0E0E0E',
+		black: '#000',
+		primary: '#E9BF99',
+		success: '#83C778',
+		destructive: '#F65F5F',
+		verifiedBlue: '#466EFC',
+	},
+	margins: {
+		xxxs: 2,
+		xxs: 8,
+		xs: 16,
+		sm: 24,
+		md: 32,
+		lg: 40,
+		xl: 48,
+		xxl: 56,
+	},
+	padding: {
+		xxxs: 2,
+		xxs: 8,
+		xs: 16,
+		sm: 24,
+		md: 32,
+		lg: 40,
+		xl: 48,
+		xxl: 56,
+	},
+} as const;
--- unistyles.ts
+++ unistyles.ts
@@ -1,18 +1,16 @@
-import { UnistylesRegistry } from 'react-native-unistyles'
-import { darkTheme } from './theme'
+import { UnistylesRegistry } from 'react-native-unistyles';
+import { darkTheme } from './theme';
 
 type AppThemes = {
-    dark: typeof darkTheme
-}
+	dark: typeof darkTheme;
+};
 
 declare module 'react-native-unistyles' {
-    export interface UnistylesThemes extends AppThemes { }
+	export interface UnistylesThemes extends AppThemes {}
 }
 
-UnistylesRegistry
-    .addThemes({
-        dark: darkTheme,
-    })
-    .addConfig({
-        adaptiveThemes: true
-    })
+UnistylesRegistry.addThemes({
+	dark: darkTheme,
+}).addConfig({
+	adaptiveThemes: true,
+});
