b37deda6da340c1a7293066323906ec6c0700365
Aman Kumar <aman.kumar@touch.black>
2024-04-10 15:05:21 +0530

    Revert "MINOR: Button Component added and Splashscreen added for iOS"
    This reverts commit 2533687384c53f5c92e063d4838cf7d88e4a15b8.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,51 +1,51 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import { Dimensions, Text, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 export type ButtonProps = {
-    onPress: () => void;
-    text: string;
-    color?: string;
-    textColor?: string;
-    buttonWidth?: number;
+	onPress: () => void;
+	text: string;
+	color?: string;
+	textColor?: string;
 };
 
-const stylesheet = createStyleSheet(theme => ({
-    button: {
-        paddingVertical: 12,
-        width: Dimensions.get('screen').width - 16,
-        alignSelf: 'flex-start',
-        flexGrow: 0,
-        backgroundColor: 'purple',
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 18,
-        fontWeight: '500',
-        textAlign: 'center',
-        lineHeight: 24,
-    },
-    buttonContainer: {
-        alignItems: 'flex-start',
-        flex: 1,
-        justifyContent: 'flex-end',
-        marginBottom: 1,
-    },
-}));
+const stylesheet = createStyleSheet({
+	button: {
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		borderRadius: 4,
+		alignSelf: 'flex-start',
+		flexGrow: 0,
+		backgroundColor: 'purple',
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold',
+	},
+	buttonContainer: {
+		alignItems: 'flex-start',
+		flex: 1,
+	},
+});
 
-export const MyButton = ({ text, onPress, color, textColor, buttonWidth }: ButtonProps) => {
-    const { styles } = useStyles(stylesheet);
+export const MyButton = ({ text, onPress, color, textColor }: ButtonProps) => {
+	const { styles } = useStyles(stylesheet);
 
-    return <SafeAreaView style={styles.buttonContainer}>
-        <TouchableOpacity
-            style={[styles.button, !!color && { backgroundColor: color }, !!buttonWidth && { width: buttonWidth }]}
-            onPress={onPress}
-            activeOpacity={0.8}>
-            <Text style={[styles.buttonText, !!textColor && { color: textColor }]}>
-                {text}
-            </Text>
-        </TouchableOpacity>
-    </SafeAreaView>
+	return (
+		<View style={styles.buttonContainer}>
+			<TouchableOpacity
+				style={[styles.button, !!color && { backgroundColor: color }]}
+				onPress={onPress}
+				activeOpacity={0.8}>
+				<Text
+					style={[
+						styles.buttonText,
+						!!textColor && { color: textColor },
+					]}>
+					{text}
+				</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
